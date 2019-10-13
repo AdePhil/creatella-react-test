d19a775c9260a0e63b896366f189ead8a0887ec5
Adebisi Ahmed <adebisi.ahmed10@gmail.com>
2019-10-14 00:41:43 +0100

    date format implementation  and currecy format function rename

--- public/front-end/src/components/Product/index.js
+++ public/front-end/src/components/Product/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import "./index.scss"
-import { currencyFormat } from "../../helpers/index";
+import { currencyFormatter, dateFormatter } from "../../helpers/index";
 const Product = ({product}) => {
   return (
     <div className="product">
       <div className="product__face" style={{ fontSize: product.size }}>{product.face}</div>
-      <div className="product__price">{ currencyFormat(product.price) }</div>
-      <div className="product__date"></div>
+      <div className="product__price">{ currencyFormatter(product.price) }</div>
+      <div className="product__date">Listed: {dateFormatter(product.date)}</div>
     </div>
   );
 };
--- public/front-end/src/helpers/index.js
+++ public/front-end/src/helpers/index.js
@@ -1,4 +1,37 @@
-export const currencyFormat = (amountInCents) => {
+export const currencyFormatter = (amountInCents) => {
   const amountInDollars = amountInCents * 1.0 / 100;
   return  amountInDollars.toLocaleString("en-US", {style:"currency", currency:"USD"});
+}
+
+export const dateFormatter = (dateString) => {
+  const dateProductWasAdded = new Date(dateString);
+  const today = new Date();
+  const timeDifference = today.getTime() - dateProductWasAdded.getTime();
+  const daysDifference = Math.round(timeDifference / (1000 * 3600 * 24));
+  if(daysDifference > 7 ){
+    return defaultDateFormatter(dateString);
+  }
+  return `${daysDifference} day${daysDifference > 1 ? 's' : ''} ago`;
+}
+
+export const defaultDateFormatter = (dateString) => {
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+  ];
+  const date = new Date(dateString);
+  let formattedDate = `${
+    months[date.getMonth()]
+  } ${date.getDate()}, ${date.getFullYear()}`;
+  return formattedDate;
 }
