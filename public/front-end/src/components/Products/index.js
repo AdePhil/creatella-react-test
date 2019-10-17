d90fbf24d2e05aee361a38b769d226b829fbd9ee
Adebisi Ahmed <adebisi.ahmed10@gmail.com>
2019-10-17 20:48:51 +0100

    insert ads after every 20 products fetched

--- public/front-end/src/components/Products/index.js
+++ public/front-end/src/components/Products/index.js
@@ -4,7 +4,18 @@ import Product from "../Product/index";
 
 const Products = ({products}) =>  (
       <div className="products">
-        {products.map(product => {
+        {products.map((product, i) => {
+          if(i % 20 === 0 && i !== 0) {
+            return (
+            <React.Fragment key={product.id}>
+            <div className="products__ad" >
+              <img  className="products__ad-img" src={`/ads/?r=${Math.floor(Math.random()*1000)}`} alt="An ad is placed here" />
+              <p className="text-center">A word from our sponsors</p>
+              </div>
+               <Product product={product} />
+            </React.Fragment>
+            )
+          }
           return <Product product={product} key={product.id} />
         })}
       </div>
