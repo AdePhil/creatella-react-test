813343117a835cd416171a04df4f662e738e3a7b
Adebisi Ahmed <adebisi.ahmed10@gmail.com>
2019-10-17 20:50:12 +0100

    added a rest button to reset filters

--- public/front-end/src/Pages/ProductsPage/index.js
+++ public/front-end/src/Pages/ProductsPage/index.js
@@ -9,12 +9,13 @@ const ProductsPage = () => {
   const [products, setProducts] = useState([]);
   const [page, setPage] = useState(1);
   const [sortBy, setSortBy] = useState('');
+  const [reset, setReset] = useState(false);
 
   useEffect(() => {
     getProducts({page, sortBy});
   }, [sortBy]);
 
-  function getProducts({page, sortBy}) {
+  const  getProducts = ({page, sortBy}) => {
      fetchProducts({page, sortBy}).then(response => {
        const newProducts = [...products, ...response.data];
       setProducts(newProducts);
@@ -23,30 +24,45 @@ const ProductsPage = () => {
     });
   }
 
-  function sort(e) {
+  const sort = (e) => {
     setProducts([]);
     setSortBy(e.target.value);
+    setPage(1);
+    setReset(true)
   }
+
+  const resetFilters = () => {
+    setProducts([]);
+    setSortBy('');
+  }
+
+
 
   return (
     <div className="products-page">
-      <h3 className="products-page__heading">Ascii Faces</h3>
-         <p className="text-center products-page__sub-heading">Buy the best and the Cheapest ascii faces in the market.</p>
+      <div><h3 className="products-page__heading">Products Grid</h3>
+         <p className="text-center products-page__sub-heading">Here you're sure to find a bargain on some of the finest ascii available to purchase. Be sure to peruse our selection of ascii faces in an exciting range of sizes and prices.</p></div>
       <div className="products-page__container">
         <div className="products-page__filters">
+          <div className="products-page__filters-container">
+          <div className="products-page__filters-group">
           <h3 className="products-page__filters-heading">Sort By</h3>
-          <label className="control control--radio">Smallest First
-            <input type="radio" name="radio" value="size" id="size" onChange={e => sort(e) } />
+          <label className="control control--radio">Size
+            <input type="radio" name="filter" value="size" id="size" onChange={e => sort(e) } />
             <div className="control__indicator"></div>
           </label>
-           <label className="control control--radio">Cheapest First
-            <input type="radio" name="radio" value="price" id="price" onChange={e => sort(e) }  />
+           <label className="control control--radio">Price
+            <input type="radio" name="filter" value="size" id="size" onChange={e => sort(e) } checked={sortBy !== ''} />
+            <input type="radio" name="filter" value="price" id="price" onChange={e => sort(e) } checked={sortBy !== ''}  />
             <div className="control__indicator"></div>
           </label>
            <label className="control control--radio">Id
-            <input type="radio" name="radio" value="id" id="id" onChange={e => sort(e) }  />
+            <input type="radio" name="filter" value="id" id="id" onChange={e => sort(e) } checked={sortBy !== ''} />
             <div className="control__indicator"></div>
           </label>
+          { sortBy !== '' ? <button onClick={() => resetFilters()} className="reset">Reset</button> : null }
+          </div>
+          </div>
         </div>
         <InfiniteScroll
           dataLength={products.length} 
@@ -57,20 +73,11 @@ const ProductsPage = () => {
           hasMore={true}
           loader={<div className="loader"><Loader /></div>}
           endMessage={
-            <p style={{textAlign: 'center'}}>~ end of catalogue ~</p>
+            <p class="text-center">~ end of catalogue ~</p>
           }>
              <Products products={products} />
         </InfiniteScroll>
-        <div className="products-page__ad">
-          <div className="products-page__ad-container">
-            <div className="products-page__ad-group">
-            <img className="ad" src={`/ads/?r=${Math.floor(Math.random()*1000)}`} />
-            { products.length > 0 ? <p>But first, a word from our sponsors</p> : null }
-            </div>
-          </div>
-        </div>
       </div>
-      
     </div>
   );
 };
